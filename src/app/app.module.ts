1e8029201661b4d8fa213632bf4445d221590681
MaximeChone <31918084+MaximeChone@users.noreply.github.com>
2021-11-29 17:36:05 +0100

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'quizzine-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  get appName() : string {
+    return "Quizzine";
+  }
+
+}
